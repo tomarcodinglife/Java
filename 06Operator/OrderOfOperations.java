836d10cd25566a1a998458d426a8e9584124a245
Sujit Tomar <tomarcodinglife@gmail.com>
2025-12-25 21:20:29 +0530

    updated

---
+++ 06Operator/OrderOfOperations.java
@@ -0,0 +1,10 @@
+
+public class OrderOfOperations {
+
+    public static void main (String[] args) {
+        System.out.println(10 - 5 * 4);
+        System.out.println(10 * 5 * 2 + 5 / 2 - 2); // BODMAS Rule Follow
+        System.out.println(10 / (4 / 2)); // BODMAS Rule Follow
+    }
+
+}
