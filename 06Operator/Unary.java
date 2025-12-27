c9b30590957d8a6feca784d79add276aeb21f836
Sujit Tomar <tomarcodinglife@gmail.com>
2025-12-27 21:01:49 +0530

    updated

--- 06Operator/Unary.java
+++ 06Operator/Unary.java
@@ -19,5 +19,8 @@ public class Unary {
         int d = c--;
         System.out.println(" d : " + d);
 
+        int e = d;
+        System.out.print("e : " + e);
+
     }
 }
---
+++ 06Operator/challanges/Airthmetic.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Airthmetic {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter First Number = ");
+        int firstNumber = input.nextInt();
+
+        System.out.print("Enter Second Number = ");
+        int secondNumber = input.nextInt();
+
+        
+
+    }
+
+}
