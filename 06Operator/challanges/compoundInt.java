dc5d3dd3327d0d9b3876fe0f5767032e6e42ed51
Sujit Tomar <tomarcodinglife@gmail.com>
2025-12-31 21:45:07 +0530

    updated

--- 06Operator/challanges/compoundInt.java
+++ 06Operator/challanges/compoundInt.java
@@ -3,9 +3,16 @@ import java.util.Scanner;
 public class compoundInt {
 
     public static void main(String[] args) {
-
+        System.out.println("Welcome to Compund Interest Calculator");
         Scanner input = new Scanner(System.in);
-        
+        System.out.print("Enter Principal Amount : ");
+        double principalAmount = input.nextDouble();
+        System.out.print("Enter Rate of Interest : ");
+        double rateOfInterest = input.nextDouble();
+        System.out.print("Enter Time Duration in Year : ");
+        double timeDuration = input.nextDouble();
+        double CompundInt = principalAmount * (1 + rateOfInterest / 100) * timeDuration; 
+        System.out.print("Result : " + CompundInt);
 
     }
 }
