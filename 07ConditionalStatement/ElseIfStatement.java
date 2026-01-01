3e1715d1be840c7bbb850a768db1153f2ae549e4
Sujit Tomar <tomarcodinglife@gmail.com>
2026-01-01 18:57:39 +0530

    updated

--- 07ConditionalStatement/ElseIfStatement.java
+++ 07ConditionalStatement/ElseIfStatement.java
@@ -5,7 +5,53 @@ public class ElseIfStatement {
     public static void main (String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.print("welcome to Tomar game");
+        System.out.println("welcome to Tomar Calculator");
+        System.out.print("What's Your Name : ");
+        String UserName = input.nextLine();
+        System.out.print("Type of Calcuting (Add/Subtract/Count/Devide) : ");
+        String Caltype = input.nextLine();
+
+        if (Caltype.equalsIgnoreCase("Add")) {
+            System.out.println("Welcome to Add Calculator " + UserName);
+            System.out.print("Enter First Number : ");
+            double firstNumber = input.nextDouble();
+            System.out.print("Enter Second Number : ");
+            double secondNumber = input.nextDouble();
+
+            System.out.print("Total : " + (firstNumber + secondNumber));
+
+        }else if(Caltype.equalsIgnoreCase("Subtract")) {
+
+            System.out.println("Welcome to Subtract Calculator " + UserName);
+            System.out.print("Enter First Number : ");
+            double firstNumber = input.nextDouble();
+            System.out.print("Enter Second Number : ");
+            double secondNumber = input.nextDouble();
+
+            System.out.print("Total : " + (firstNumber - secondNumber));
+
+        }else if(Caltype.equalsIgnoreCase("Count")) {
+            System.out.println("Welcome to Count Calculator " + UserName);
+            System.out.print("Enter First Number : ");
+            double firstNumber = input.nextDouble();
+            System.out.print("Enter Second Number : ");
+            double secondNumber = input.nextDouble();
+
+            System.out.print("Total : " + (firstNumber * secondNumber));
+
+        }else if (Caltype.equalsIgnoreCase("Devide")) {
+            System.out.println("Welcome to Devide Calculator " + UserName);
+            System.out.print("Enter First Number : ");
+            double firstNumber = input.nextDouble();
+            System.out.print("Enter Second Number : ");
+            double secondNumber = input.nextDouble();
+
+            System.out.print("Total : " + (firstNumber / secondNumber));
+        }else {
+            System.out.print("Please Enter Correct Answer");
+        }
+
+
 
 
 
