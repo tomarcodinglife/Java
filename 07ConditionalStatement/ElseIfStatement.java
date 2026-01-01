095221139e1ee4901eb7ca745575cf0409c10836
Sujit Tomar <tomarcodinglife@gmail.com>
2026-01-01 18:27:24 +0530

    updated

--- 07ConditionalStatement/ElseIfStatement.java
+++ 07ConditionalStatement/ElseIfStatement.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 
 public class ElseIfStatement {
 
+    public static void main (String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("welcome to Tomar game");
+
+
+
+    }
      
 }
