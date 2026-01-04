03f9739e100112c76256c930c5c3003ad0797b60
Sujit Tomar <tomarcodinglife@gmail.com>
2026-01-04 20:12:21 +0530

    updated

---
+++ 10Loop/WhileLoop.java
@@ -0,0 +1,22 @@
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        
+        int num = 1; //initialization
+            while (num <= 10) { // COndition 
+                System.out.print("Sujit Tomar"); // actual work
+                num += 1; // update condition
+            }
+        
+
+        int count = 100;
+            while (count >= 10) {
+                System.out.println("Sujit Tomar" + count);
+                count -= 1;
+                
+            }
+
+    }
+
+}
---
+++ 11Function/MyFunction.java
@@ -0,0 +1,18 @@
+
+public class MyFunction {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome");
+        name();
+
+    }
+
+
+    // this is function
+    public static void name() {
+        System.out.println("Sujit Tomar");
+    }
+
+
+}
