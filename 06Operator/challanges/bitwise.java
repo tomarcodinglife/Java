9e40d4f5d0124825c32a421f13cef9d97c32c259
Sujit Tomar <tomarcodinglife@gmail.com>
2026-01-04 18:09:14 +0530

    UPDATED

--- 06Operator/challanges/bitwise.java
+++ 06Operator/challanges/bitwise.java
@@ -1,9 +1,29 @@
-
+import java.util.Scanner;
 
 public class bitwise {
 
     public static void main(String[] args) {
         
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter First Number : ");
+        int FirstNumber = input.nextInt();
+        System.out.print("Enter Second Number : ");
+        int SecondNumber = input.nextInt();
+
+        int ANDOperator = FirstNumber & SecondNumber;
+        int OROperator = FirstNumber | SecondNumber;
+        int XOROperator = FirstNumber ^ SecondNumber;
+        int NOTOperator = ~FirstNumber;
+        int LeftShiftOperator = FirstNumber << SecondNumber;
+        int RightShiftOperator = FirstNumber >> SecondNumber;
+
+        System.out.println("AND Operator : " + ANDOperator);
+        System.out.println("OR Operator : " + OROperator);
+        System.out.println("XOR Operator : " + XOROperator);
+        System.out.println("NOT Operator : " + NOTOperator);
+        System.out.println("Left Shift Operator : " + LeftShiftOperator);
+        System.out.println("ARight Shift Operator : " + RightShiftOperator);
+
     }
 
 
---
+++ 07ConditionalStatement/challanges/OddEvenWithBitwise.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class OddEvenWithBitwise {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int number = input.nextInt();
+
+        if ((number & 1) == 1) {
+            System.out.println("Number is Odd");
+        }else{
+            System.out.println("Number is Even");
+        }
+
+
+    }
+
+}
