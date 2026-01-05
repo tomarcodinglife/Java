dbce677f6459c12e6324fbede40cfbb76cb94f66
Sujit Tomar <tomarcodinglife@gmail.com>
2026-01-05 21:31:48 +0530

    updated

--- 11Function/MyFunction.java
+++ 11Function/MyFunction.java
@@ -3,8 +3,9 @@ public class MyFunction {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome");
-        name();
+        // System.out.println("Welcome");
+        // name();
+        printPattern();
 
     }
 
@@ -12,6 +13,22 @@ public class MyFunction {
     // this is function
     public static void name() {
         System.out.println("Sujit Tomar");
+    }
+
+    public static void printPattern(){
+
+        int star = 0;
+        while (star < 5) {
+            System.out.print("*");
+            int space = 0;
+            while (space < star) {
+                System.out.print(" *");
+                space++;
+            }
+            System.out.println();
+            star++;
+        }
+
     }
 
 
