37c71e5d06eb79f556ac34635fd0f8eec8a5961f
Sujit Tomar <tomarcodinglife@gmail.com>
2026-01-09 21:36:28 +0530

    updated

--- 11Function/challanges/factorials.java
+++ 11Function/challanges/factorials.java
@@ -1,7 +1,15 @@
 package challanges;
 
+import java.util.Scanner;
+
 public class factorials {
     
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner("System.in");
+        System.out.print("Welcome to ");
 
-    
+
+    }
+
 }
