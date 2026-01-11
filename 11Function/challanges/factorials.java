e2496ad8a8a834f845e434e619f3a037f5ae434e
Sujit Tomar <tomarcodinglife@gmail.com>
2026-01-11 18:28:06 +0530

    updated

--- 11Function/challanges/factorials.java
+++ 11Function/challanges/factorials.java
@@ -3,13 +3,24 @@ package challanges;
 import java.util.Scanner;
 
 public class factorials {
-    // Sujit
+    
     public static void main(String[] args) {
         
-        Scanner input = new Scanner("System.in");
-        System.out.print("Welcome to ");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Welcome to Factorials");
+        System.out.print("Please Enter Your Number : ");
+        int num = input.nextInt();
+        int sum = sumOfDigits(num);
+        System.out.print("Sum of Digit is" + sum);
+    }   
 
+    public static int sumOfDigits(int num) {
+        int sum = 0;
 
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
     }
-
 }
