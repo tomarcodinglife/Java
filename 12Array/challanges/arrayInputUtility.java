1ecbe016f8fbdbef68c73e10acb2bfd26a474627
Sujit Tomar <tomarcodinglife@gmail.com>
2026-01-24 19:18:23 +0530

    updated

---
+++ 12Array/challanges/arrayInputUtility.java
@@ -0,0 +1,24 @@
+package challanges;
+import java.util.Scanner;
+
+class arrayInputUtility{
+
+    public static int[] inputArray() {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Number the No of ELement : ");
+        int arraySize = input.nextInt();
+        
+        int[] nums = new int[arraySize];
+
+        int i = 0;
+        while (i < arraySize) {
+            System.out.print("Enter Number " + (i + 1) + ": ");
+            nums[i] = input.nextInt();
+            i++;
+        }
+
+        return nums;
+    }
+}
--- 12Array/challanges/sumAndAverage.java
+++ 12Array/challanges/sumAndAverage.java
@@ -1,6 +1,37 @@
 package challanges;
 
+import java.util.Scanner;
+
 public class sumAndAverage {
     
-    // Sujit Kumar Singh
+    public static void main(String[] args){
+        
+        System.out.print("Sum and Average");
+        int[] myInputArray = arrayInputUtility.inputArray();
+        
+        long total = sum(myInputArray);
+        double avg = average(myInputArray);
+
+        System.out.println(total);
+        System.out.println(avg);
+
+    }
+
+    public static long sum (int[] numsArray){
+        long sum = 0;
+        int i = 0;
+
+        while (i < numsArray.length) {
+            sum += numsArray[i];
+            i++;
+        }
+        return sum;
+    }
+
+    public static double average (int[] numsArray){
+        long sum = sum(numsArray);
+        double average = (double) (sum / numsArray.length);
+        return average;
+    }
+    
 }
