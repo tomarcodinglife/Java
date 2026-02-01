ac127c42ffc7118798fca06d813eca8ed360bf39
Sujit Tomar <tomarcodinglife@gmail.com>
2026-02-01 17:58:02 +0530

    updated

--- 12Array/challanges/arrayInputUtility.java
+++ 12Array/challanges/arrayInputUtility.java
@@ -6,12 +6,9 @@ class arrayInputUtility{
     public static int[] inputArray() {
 
         Scanner input = new Scanner(System.in);
-
         System.out.print("Enter Number the No of Element : ");
         int arraySize = input.nextInt();
-        
         int[] nums = new int[arraySize];
-
         int i = 0;
         while (i < arraySize) {
             System.out.print("Enter Number " + (i + 1) + ": ");
--- 12Array/challanges/mergeTwoSortedArray.java
+++ 12Array/challanges/mergeTwoSortedArray.java
@@ -1,12 +1,56 @@
+import java.lang.reflect.Array;
 
 public class mergeTwoSortedArray{
 
     public static void main (String[] args) {
+        System.out.println("Welcome to Merge Two Sorted Array");
+
+        int[] Array1 = arrayInputUtility.inputArray();
+        int[] Array2 = arrayInputUtility.inputArray();
         
+        int[] result = mergeArray(Array1, Array2);       
         
+        displayArr(result);
 
     }
 
+    public static int[] mergeArray(int[]Array1, int[] Array2){
+        int[] NewArray = new int[Array1.length + Array2.length];
 
+        int i = 0, j=0, k=0;
+
+        while (i < Array1.length && j < Array2.length) {
+            if (Array1[i] <= Array2[j]) {
+                NewArray[k] = Array1[i];
+                i++;
+            }else{
+                NewArray[k] = Array2[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < Array1.length) {
+            NewArray[k] = Array1[i];
+            i++;
+            k++;
+        }
+
+        while (j < Array2.length) {
+            NewArray[k] = Array2[j];
+            j++;
+            k++;
+        }
+        return NewArray;
+    }
+
+    public static void displayArr(int[] Array){
+        int i = 0;
+
+        while (i < Array.length) {
+            System.out.print(Array[i] + " ");
+            i++;
+        }
+    }
 
 }
