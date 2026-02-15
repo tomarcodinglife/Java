b97d8a1bd1cfe27477696569e0ab18ebd4f60eef
Sujit Tomar <tomarcodinglife@gmail.com>
2026-02-15 11:37:52 +0530

    updated

--- 14ControlStatement/chalange/scoreCheck.java
+++ 14ControlStatement/chalange/scoreCheck.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 public class scoreCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+        System.out.println("Welcome to Score Check Method");
+        System.out.println("Enter the own marks: ");
+
+        int num = input.nextInt();
+
+        if (num < 80) {
+            System.out.println("High");
+        }else if (num > 50 && num < 80) {
+            System.out.println("Moderate");
+        }else if (num < 50) {
+            System.out.println("Low");
+        }else{
+            System.out.println("Invilid");
+        }
+
     }
 }
