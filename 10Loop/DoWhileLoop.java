1b4ec91e1413d11a660e8f797b72e74b2e553fce
Sujit Tomar <tomarcodinglife@gmail.com>
2026-02-15 20:23:31 +0530

    updated

--- 10Loop/DoWhileLoop.java
+++ 10Loop/DoWhileLoop.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 
 public class DoWhileLoop {
-    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Age Checker");
+
+        int age;
+        do{
+            System.out.println("Enter the own Age : ");
+            age = input.nextInt();
+            if (age < 0 || age > 150) {
+                System.out.println("Invalid Entry");
+            }else if (age <= 18) {
+                System.out.println("Your are Child");
+            }else if(age <= 44){
+                System.out.println("Your are young");
+            }else{
+                System.out.println("Your are old");
+            }
+        }while(age < 0 || age > 150);
+        input.close();
+
+    }
 }
--- 10Loop/forEachLoop.java
+++ 10Loop/forEachLoop.java
@@ -1,4 +1,26 @@
 
 public class forEachLoop {
-    
+    public static void main(String[] args) {
+        String[] array  = new String[]{
+        "Rohit", "Sohan", "Sameer", "Aman", "Rahu", "Raman", "Shivam"
+        };
+
+        printArray(array);
+        System.out.println();
+        PrintForEach(array);
+    }
+
+    public static void printArray(String[] Array){
+        for(int i =0; i < Array.length; i++){
+            System.out.print(Array[i] + " ");
+        }
+    }
+
+    public static void PrintForEach(String[] Array){
+        for(String name : Array){
+            System.out.print(name + " ");
+        }
+    }
+
+
 }
