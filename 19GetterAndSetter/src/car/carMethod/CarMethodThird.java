5a5d8b2758953eb03eed9bd4e14f260e0669080e
Sujit Tomar <tomarcodinglife@gmail.com>
2026-03-07 09:16:50 +0530

    updated

--- 19GetterAndSetter/src/car/carMethod/CarMethodThird.java
+++ 19GetterAndSetter/src/car/carMethod/CarMethodThird.java
@@ -1,5 +1,50 @@
 package car.carMethod;
 
 public class CarMethodThird {
+    private final String carModel;
+    private final String carManufacturerCompany;
+    private final int carManufacturingYear;
+    private String carColour;
+    private String carFuelType;
+    private int carCurrentFuelLevel;
+    private int carPrice;
+    private final int carSeat;
+    private int carInsurance;
+    private int carRoadTax;
+    private boolean carPollution;
+    private boolean carInsuranceValid;
+    private boolean carRoadTaxValid;
+    private String carInsurenceCompany;
+
+    CarMethodThird(String carModel, String carManufactureCompany, int carManufacturingYear, int carSeat){
+        this.carModel = carModel;
+        this.carManufacturerCompany = carManufactureCompany;
+        this.carManufacturingYear = carManufacturingYear;
+        this.carSeat = carSeat;
+
+        // Default Value
+        this.carColour = "Black";
+        this.carFuelType = "Petrol";
+        this.carCurrentFuelLevel = 2;
+        System.out.printf("Object Created by Car Method Third");
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder myStr = new StringBuilder();
+        myStr.append("Car Model : ").append(carModel).append("\n");
+        myStr.append("Car Manufacture Company : ").append(carManufacturerCompany).append("\n");
+        myStr.append("Car Manufacture Year: ").append(carManufacturingYear).append("\n");
+        myStr.append("Car Seat: ").append(carSeat).append("\n");
+        return myStr.toString();
+    }
+
+
+
+
+
+
+
+
     
 }
