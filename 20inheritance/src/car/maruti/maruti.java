09a403fb0840815aefd5833e0c3ea795d69086c0
Sujit Tomar <tomarcodinglife@gmail.com>
2026-03-09 20:05:08 +0530

    updated

--- 20inheritance/src/car/maruti/maruti.java
+++ 20inheritance/src/car/maruti/maruti.java
@@ -1,7 +1,22 @@
 package car.maruti;
 
-import car.method;
+import car.method.Car;
 
 public class maruti extends Car{
-    
+    private int price;
+
+    public maruti(String colour, int seatCapacity, String model, int manufacturingYear, int vehicle, int price){
+        // you can define in two types
+        // super("Red", 5, "Swift", 2024, 4);
+        super(colour, seatCapacity, model, manufacturingYear, vehicle);
+        this.price = price;
+    }
+
+    public static void main(String[] args) {
+        maruti Swift = new maruti("Red", 5, "Swift", 2024, 4, 50000);
+        System.out.println(Swift);
+        System.out.println(Swift.price);
+
+    }
+
 }
--- 20inheritance/src/car/method/Car.java
+++ 20inheritance/src/car/method/Car.java
@@ -1,16 +1,34 @@
 package car.method;
 
 public class Car {
-    protected String colour;
-    protected double price;
-    protected int seatCapacity;
+    private String colour;
+    private int seatCapacity;
+    private String model;
+    private int manufacturingYear;
+    private int vehicle;
+     
+
+    public Car(String colour, int seatCapacity, String model, int manufacturingYear, int vehicle){
+        this.colour = colour;
+        this.seatCapacity = seatCapacity;
+        this.model = model;
+        this.manufacturingYear = manufacturingYear;
+        this.vehicle = vehicle;
+    }
+
+    public String toString(){
+        StringBuilder str = new StringBuilder();
+        str.append("Model : ").append(model).append("\n");
+        return str.toString();
+    }
+
 
     public int getSeatCapacity(){
         return seatCapacity;
     }
 
     public void setSeatCapacity(int seat){
-        if(seat != 4){
+        if(seat <= 0){
             System.out.print("Invalid Seat Capacity");
         }else{
              seatCapacity = seat;
