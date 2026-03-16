c6caa48eac52ab8ba2b31e8417e65bba6da33a55
Sujit Tomar <tomarcodinglife@gmail.com>
2026-03-16 09:27:02 +0530

    UPDATED

---
+++ 25Interfaces/chalanges/shape/Square.java
@@ -0,0 +1,22 @@
+package shape;
+
+public class Square extends Shape {
+    private double sideLength;
+
+    public Square(double sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    public double getSideLength() {
+        return sideLength;
+    }
+
+    public void setSideLength(double sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    public double area() {
+        return Math.pow(sideLength, 2);
+    }
+}
---
+++ 25Interfaces/chalanges/shape/test.java
@@ -0,0 +1,17 @@
+package shape;
+
+public class test {
+
+    public static void main(String[] args) {
+        // Circle
+        Circle circle = new Circle(5);
+        System.out.println("Circle Area: " + circle.area());
+
+        // Square
+        Square square = new Square(4);
+        System.out.println("Square Area: " + square.area());
+
+
+    }
+    
+}
