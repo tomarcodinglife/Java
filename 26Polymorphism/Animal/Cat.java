13fd3c2e08e2a57549df8ad2e39e43a25f15eb58
Sujit Tomar <tomarcodinglife@gmail.com>
2026-03-22 21:18:04 +0530

    UPDATED

--- 26Polymorphism/Animal/Cat.java
+++ 26Polymorphism/Animal/Cat.java
@@ -2,14 +2,17 @@ package Animal;
 
 public class Cat extends Animal {
     
+    @Override
     void eat(){
         System.out.println("Cat is eating");
     }
 
+    @Override
     void sleep(){
         System.out.println("Cat is sleeping");
     }
 
+    @Override
     void makeSound(){
         System.out.println("Cat is meowing");
     }
--- 26Polymorphism/Animal/Dog.java
+++ 26Polymorphism/Animal/Dog.java
@@ -1,15 +1,17 @@
 package Animal;
 
 public class Dog extends Animal {
-    
+    @Override
     void eat(){
         System.out.println("Dog is eating");
     }
 
+    @Override
     void sleep(){
         System.out.println("Dog is sleeping");
     }
 
+    @Override
     void makeSound(){
         System.out.println("Dog is barking");
     }
