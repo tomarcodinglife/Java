b1b9cace80bcb40f7394d939994638e22413b244
Sujit Tomar <tomarcodinglife@gmail.com>
2026-03-28 09:12:13 +0530

    updated

--- 27ExceptionAndFiles/TryCatch/Calculator.java
+++ 27ExceptionAndFiles/TryCatch/Calculator.java
@@ -7,22 +7,31 @@ public class Calculator{
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to DIvision Calculator");
-
-        System.out.println("Please Enter your two Numbers : ");
-
-        System.out.print("Enter First Number : ");
-        int first = input.nextInt();
-        System.out.print("Enter Second Number : ");
-        int second = input.nextInt();
+        System.out.println("Welcome to Division Calculator");
 
 
         try {
+            System.out.print("Enter First Number : ");
+            int first = input.nextInt();
+
+            System.out.print("Enter Second Number : ");
+            int second = input.nextInt();
+
             int result = first / second;
-            System.out.printf("Result is %d", result);
+            System.out.println("Result is " +  result);
+            
+
         } catch (ArithmeticException e) {
             System.out.printf(" %S, Divide by Zero, Enter Valid Value", e.getMessage());
+
+        } catch (Exception e) {
+            System.out.println("Invalid Input");
+            
+        } finally{
+            System.out.println("Calculator Closed");
         }
+
+        
 
         
 
