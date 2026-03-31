79a71ca26d1ad5b0ec817fbfa1a61c443a0fac33
Sujit Tomar <tomarcodinglife@gmail.com>
2026-03-31 21:32:37 +0530

    updated

--- 31Unboxing/Unboxing.java
+++ 31Unboxing/Unboxing.java
@@ -4,7 +4,13 @@ public class Unboxing {
 
     public static void main(String[] args) {
         
-        // Wrapper Class me 
+        // Wrapper Class me premitive data types ko objects me convert karne ko autoboxing kehte hain, aur objects ko primitive data types me convert karne ko unboxing kehte hain. Unboxing ka use tab hota hai jab aapko wrapper class ke object se primitive value chahiye hoti hai.
+
+        // Unboxing Example:
+        Integer num1 = 10; // Autoboxing: int to Integer
+        int a = num1; // Unboxing: Integer to int
+        System.out.println("Unboxed value: " + a); // Output: Unboxed value
+        
 
 
 
