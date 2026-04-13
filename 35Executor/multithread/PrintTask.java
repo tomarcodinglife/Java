1fbf168800024715ca832e130a20bc0f825896e5
Sujit Tomar <tomarcodinglife@gmail.com>
2026-04-13 20:48:17 +0530

    updated

---
+++ 35Executor/multithread/PrintTask.java
@@ -0,0 +1,19 @@
+package multithread;
+
+public class PrintTask implements Runnable {
+
+    private final char targetChar;
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 100; i++) {
+            System.out.printf("%d%c", i, targetChar);  
+        }
+        System.out.printf("\n %s %c Thread is task completed", Thread.currentThread().getName(), targetChar);
+    }
+
+    public PrintTask(char targetChar) {
+        this.targetChar = targetChar;
+    }
+    
+}
---
+++ 35Executor/multithread/TestMultiThread.java
@@ -0,0 +1,19 @@
+package multithread;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestMultiThread {
+    public static void main(String[] args) {
+
+        ExecutorService service = Executors.newFixedThreadPool(3);
+        PrintTask printTask1 = new PrintTask('*');
+        PrintTask printTask2 = new PrintTask('$');
+        PrintTask printTask3 = new PrintTask('#');
+
+        service.submit(printTask1);
+        service.submit(printTask2);
+        service.submit(printTask3);
+
+        service.shutdown();
+    }
+}
