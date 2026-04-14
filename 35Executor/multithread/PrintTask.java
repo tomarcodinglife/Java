9b0c2c6794b2edee854c89a56bf24da36be260e5
Sujit Tomar <tomarcodinglife@gmail.com>
2026-04-14 18:38:44 +0530

    updated

--- 35Executor/multithread/PrintTask.java
+++ 35Executor/multithread/PrintTask.java
@@ -6,14 +6,26 @@ public class PrintTask implements Runnable {
 
     @Override
     public void run() {
-        for (int i = 1; i < 100; i++) {
+
+        for (int i = 1; i <= 100; i++) {
+
+           try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             System.out.printf("%d%c", i, targetChar);  
         }
+
         System.out.printf("\n %s %c Thread is task completed", Thread.currentThread().getName(), targetChar);
+
     }
 
     public PrintTask(char targetChar) {
         this.targetChar = targetChar;
     }
+
+
     
 }
--- 35Executor/multithread/TestMultiThread.java
+++ 35Executor/multithread/TestMultiThread.java
@@ -1,9 +1,11 @@
 package multithread;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import java.util.concurrent.TimeUnit;
 
 public class TestMultiThread {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         ExecutorService service = Executors.newFixedThreadPool(3);
         PrintTask printTask1 = new PrintTask('*');
@@ -15,5 +17,12 @@ public class TestMultiThread {
         service.submit(printTask3);
 
         service.shutdown();
+        System.out.println("All tasks submitted.");
+
+        if (!service.awaitTermination(10, TimeUnit.SECONDS)) {
+            System.out.println("Time out. Forcing shutdown...");
+            service.shutdownNow();
+        }
+
     }
 }
